f06fd9d5ec625fc8874bd93e262c58e205332446
Martin <martin.detle@gmail.com>
2021-07-27 10:58:20 -0300

    Curso Angular - Clase 19 / Seccion 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NotFoundComponent } from './pages/404/not-found.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NotFoundComponent,
+    DashboardComponent,
+    BreadcrumbsComponent,
+    HeaderComponent,
+    SidebarComponent,
   ],
   imports: [
     BrowserModule
